61f41f42eac62f9640907ed9d21984fe54e840e3
trqminh <1612374@student.hcmus.edu.vn>
2020-02-09 17:36:14 +0700

    day 1 - James Ward-Prowse

--- solutions/1.py
+++ solutions/1.py
@@ -1,1 +1,16 @@
-import math
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        invert_idx = {}
+        for idx, num in enumerate(nums):
+            n = target - num
+            if n not in invert_idx:
+                invert_idx[num] = idx
+            else:
+                return [idx, invert_idx[n]]
+
+        return "No answer"
