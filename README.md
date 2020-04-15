b694225e0d52aa0731866c35b1b02204c20b7e28
trqminh <1612374@student.hcmus.edu.vn>
2020-04-16 06:13:48 +0700

    day 33 - Chris Martin

--- README.md
+++ README.md
@@ -36,5 +36,6 @@ a day a problem aim not to forget how to code
 | 31  | Diameter of Binary Tree | Medium | [Q](https://leetcode.com/problems/diameter-of-binary-tree/) | [A](/solutions/31.py) | [N](notes/31.txt) |
 | 32  | Maximum Length of Repeated Subarray | Medium | [Q](https://leetcode.com/problems/maximum-length-of-repeated-subarray/) | [A](/solutions/32.py) | [N](notes/32.txt) |
 | 33  | Intersection of Two Arrays | Easy | [Q](https://leetcode.com/problems/intersection-of-two-arrays/) | [A](/solutions/33.py) | [N](notes/33.txt) |
+| 34  |  |  | [Q]() | [A](/solutions/34.py) | [N](notes/34.txt) |
 
 \* indicates that I can solve the problem optimally
--- solutions/33.py
+++ solutions/33.py
@@ -6,12 +6,26 @@ class Solution(object):
         :rtype: List[int]
         """
         nums1.sort()
-        nums1.sort()
+        nums2.sort()
         intersection = []
 
         i1, i2 = 0, 0
 
-        if nums1[i1] == nums2[i2]:
-            intersection.append(sums:wq
+        while i1 < len(nums1) and i2 < len(nums2):
+            if nums1[i1] == nums2[i2]:
+                intersection.append(nums1[i1])
+                i1+=1
+                i2+=1
+                while i1 < len(nums1) and i2 < len(nums2) and nums1[i1] == nums2[i2]:
+                    if nums1[i1] != nums1[i1 - 1]:
+                        intersection.append(nums1[i1])
+                    i1+=1
+                    i2+=1
+
+            elif nums1[i1] < nums2[i2]:
+                i1 += 1
+            else:
+                i2 += 1
 
 
+        return intersection
